57465cbbdccda9800a8dc745a6d155fa42c5195a
Cinthia M. Orona <devrider2021@gmail.com>
2024-06-20 15:07:53 -0300

    agrege recibir2.php

--- recibir.php
+++ recibir.php
@@ -1,28 +1,37 @@
 <!-- Esto es la conexion al servidor y recepcion de los datos en la DB, capturados por el formulario -->
- <!-- Por ultimo para mostrar los datos usamos el método que más nos convenga. -->
+<!-- Por ultimo para mostrar los datos usamos el método que más nos convenga. -->
 
 
 <?php
 
-$con = mysqli_connect("localhost", "root", "", "movies_cac") 
-or die("Error en la conexion servidor");
+$con = mysqli_connect("localhost", "root", "", "movies_cac")
+    or die("Error en la conexion servidor");
 
 $sql = "INSERT INTO user (name, surname, email, password, date, country) 
 VALUES ('$_POST[name]', '$_POST[surname]', '$_POST[email]', '$_POST[password]', '$_POST[date]', '$_POST[country]')";
 
 $result = mysqli_query($con, $sql) or die("Error en el query de base de datos");
 
-// mysqli_close($con);
+mysqli_close($con);
 
-echo "<p>El nombre es: $_POST[name] </p><br>";
-echo "El apellido es: $_POST[surname] <br>";
-echo "El email es: $_POST[email] <br>";
-echo "La contraseña es: $_POST[password] <br>";
-echo "La fecha es: $_POST[date] <br>";
-echo "El pais es: $_POST[country] <br>";
+//TODO: Si se quiere mostrar los datos capturados en el formulario, es necesario hacerlo en este archivo
 
-//?? Esto muestra los datos en un array
-// print_r($_POST);
 
-echo $_POST['name'] . " " . $_POST['country'];
 ?>
+
+
+<!--  
+//!! MANERA BASICA DE MOSTRAR DATOS CAPTADOS POR EL SERVER EN TEXTO PLANO
+// echo "<p>El nombre es: $_POST[name] </p><br>";
+// echo "El apellido es: $_POST[surname] <br>";
+// echo "El email es: $_POST[email] <br>";
+// echo "La contraseña es: $_POST[password] <br>";
+// echo "La fecha es: $_POST[date] <br>";
+// echo "El pais es: $_POST[country] <br>";
+-->
+
+<!-- 
+//!! MANERA DE MOSTRAR DATOS CAPTADOS POR EL SERVER EN UN ARRAY
+    //?? Esto muestra los datos en un array
+    // print_r($_POST);
+ -->
